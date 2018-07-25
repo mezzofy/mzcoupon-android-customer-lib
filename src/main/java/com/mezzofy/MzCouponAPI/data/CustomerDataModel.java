f9767249ed6399fede643d28bdc8f064b703647e
Aruna <aruna@mezzofy.com>
2018-07-25 15:16:12 +0530

    data change

--- src/main/java/com/mezzofy/MzCouponAPI/data/CustomerDataModel.java
+++ src/main/java/com/mezzofy/MzCouponAPI/data/CustomerDataModel.java
@@ -11,7 +11,6 @@ public class CustomerDataModel {
     public CustomerData getCustomer() {
         return customer;
     }
-
     public void setCustomer(CustomerData customer) {
         this.customer = customer;
     }
